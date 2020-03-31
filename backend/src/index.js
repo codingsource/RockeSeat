e0b26c55be668565252a922dc92ec5c522c2aa70
Peter Thomas <peterpetrick@gmail.com>
2020-03-31 19:41:17 -0300

    Revert "Cors"
    This reverts commit 97235e09c7695d7bae70df35951eb826c4fb27a7.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,10 @@
 const express = require('express')
-const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 
 const port = 3333
 
-
-app.use(cors())
 /**
  * permite que o servidor entenda o formato json na requisição post
  */

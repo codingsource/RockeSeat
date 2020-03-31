f546b1ce1b51b039177d9d199b9c1296c5af84c1
Peter Thomas <peterpetrick@gmail.com>
2020-03-31 11:15:03 -0300

    Adicionando modulo nodemon para reiniciar o server quando ouver alguma alteração durante a criação do projeto

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 
 const app = express()
-
 const port = 3333
+
+app.get('/', (request, response) => {
+
+  return response.send('Hello Word')
+})
 
 app.listen(port, () =>{
   console.log(`Server rodando na porta => ${port}`)

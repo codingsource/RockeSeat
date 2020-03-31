826d38a58a505b361175d031f18b7c2feeb3f12a
Peter Thomas <peterpetrick@gmail.com>
2020-03-31 12:56:21 -0300

    trabalhando com rotas

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,42 @@
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET: Buscar uma informação no back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Altear uma informação no back-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parametros:
+   * 
+   * Query Params: Parâmetros nomeados enviados na rota apos "?" (Filtros, paginação) acessa os dados atraves do request.query app.get('/users', (request, response) => {
+   *
+   *  Router Params: Paâmetros utilizados pra identificar recursos identifica um unico recurso /users/1 app.get('/users/:id', (request, response) => {
+   * (não aceita parâmetros a mais do que está sendo esperado) acessa os dados atraves do request.params
+   * 
+   * Request Body: O corpo da requisição, utilizado para criar ou alterar recursos app.get('/users', (request, response) => {
+   * acessa os dados atraves do request.body
+   */
+
 const express = require('express')
 
 const app = express()
 const port = 3333
 
-app.get('/', (request, response) => {
+/**
+ * permite que o servidor entenda o formato json na requisição post
+ */
+app.use(express.json())
+
+app.post('/users', (request, response) => {
+
+  const params = request.body
+  console.log(params)
 
   return response.json({
     evento: "OmniStack 11.0",
